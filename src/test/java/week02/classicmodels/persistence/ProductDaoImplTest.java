9dc2c6731ff0611758e523d30183c94f33cd4ace
Chealy-13 <chris.healy33@gmail.com>
2024-09-24 13:00:44 +0100

    checking test file works

---
+++ src/test/java/week02/classicmodels/persistence/ProductDaoImplTest.java
@@ -0,0 +1,13 @@
+package week02.classicmodels.persistence;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDaoImplTest {
+
+    @org.junit.jupiter.api.Test
+    void getAllProducts() {
+    assertTrue(true);
+    }
+
+
+}
